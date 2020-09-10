ddfed7f7743a5ae7434495dfe87e26deb3b1e728
SunJet Liu <sunjetliu@gmail.com>
2020-09-10 21:30:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ end
 def get_english_meaning(yaml_file, emoticon)
   # code goes here
   my_hash = load_library(yaml_file)
+  binding.pry
   # english_meaning = emoticon_lib['japanese'][emoticon]
   # puts english_meaning
 #   name = nil
