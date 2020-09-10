34ca75ca5bfd09757507ebe65d9d32d7cf350e81
SunJet Liu <sunjetliu@gmail.com>
2020-09-10 22:39:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_english_meaning(yaml_file, emoticon)
 emoticons = load_library(yaml_file)
 emoticons.each do |english_meaning, emoarr |
 binding.pry
-if emoticon == emoarr['japanese']
+if emoticon == emoarr[:japanese]
   binding.pry
 meaning = english_meaning
 binding.pry
