3760954f4886c672a355b21ca06a77b2972e239b
SunJet Liu <sunjetliu@gmail.com>
2020-09-10 21:24:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,8 @@ end
 def get_english_meaning(yaml_file ,emoticon)
   # code goes here
   emoticon_lib = load_library(yaml_file)
-  english_meaning = emoticon_lib['japanese'][emoticon]
-  puts english_meaning
+  # english_meaning = emoticon_lib['japanese'][emoticon]
+  # puts english_meaning
 #   name = nil
 # binding.pry
 # library[emo].each do |k, v| #look in japanes to find value = to then return orig key
